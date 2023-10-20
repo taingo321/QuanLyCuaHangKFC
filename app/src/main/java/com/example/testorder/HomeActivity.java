1bb133d50381109eb8599312bec5ddd0296318f9
Zebao1511 <lenguyengiabao15112003@gmail.com>
2023-10-20 14:39:34 +0700

    Thêm bottom navgition và chuyển layout từ Home cũ sang Trang Chủ Fragment

--- app/src/main/java/com/example/testorder/HomeActivity.java
+++ app/src/main/java/com/example/testorder/HomeActivity.java
@@ -2,119 +2,75 @@ package com.example.testorder;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.testorder.Fragment.GioHangFragment;
+import com.example.testorder.Fragment.LichSuFragment;
+import com.example.testorder.Fragment.TrangChuFragment;
 import com.example.testorder.Model.Products;
 import com.example.testorder.ViewHolder.ProductViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.rey.material.widget.FrameLayout;
 import com.squareup.picasso.Picasso;
 
 import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private DatabaseReference ProductsRef;
-    private RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    private FloatingActionButton fabCart, logout, profile, orders;
+
+    BottomNavigationView botView;
+    FrameLayout mFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        loadFragment(new TrangChuFragment());
 
-        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
-        recyclerView = findViewById(R.id.recycler_menu);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        botView = findViewById(R.id.bnv);
+        botView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.TrangChu){
+                    loadFragment(new TrangChuFragment());
+                } else if (item.getItemId() == R.id.LichSu) {
+                    loadFragment(new LichSuFragment());
+                } else if (item.getItemId() == R.id.GioHang) {
+                    loadFragment(new GioHangFragment());
+                }
 
-        fabCart = findViewById(R.id.fabCart);
-        logout = findViewById(R.id.logout);
-        profile = findViewById(R.id.profile);
-        orders = findViewById(R.id.orders);
+                return true;
+            }
+        });
+
+
+
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
-                .setQuery(ProductsRef, Products.class).build();
-
-        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
-            @Override
-            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
-                holder.product_Name.setText(model.getPname());
-                holder.product_Description.setText(model.getDescription());
-                holder.product_Price.setText("Price = " + model.getPrice() + "$");
-                Picasso.get().load(model.getImage()).into(holder.product_Image);
-
-                holder.itemView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(HomeActivity.this, ProductDetailsActivity.class);
-                        intent.putExtra("pid", model.getPid());
-                        startActivity(intent);
-                    }
-                });
-            }
-
-            @NonNull
-            @Override
-            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
-                ProductViewHolder holder = new ProductViewHolder(view);
-                return holder;
-            }
-        };
-        recyclerView.setAdapter(adapter);
-        adapter.startListening();
-
-        fabCart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, CartActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Paper.book().destroy();
-
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        orders.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, AdminNewOrdersActivity.class);
-                startActivity(intent);
-            }
-        });
-
+    private void loadFragment (Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frContent, fragment);
+        fragmentTransaction.commit();
     }
+
+
 }
