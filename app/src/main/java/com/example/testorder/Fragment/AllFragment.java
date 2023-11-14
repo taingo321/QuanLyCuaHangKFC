aaeac290cbaa8a6fb095526a30e475c5a2cabc5b
taingo321 <anhtaingo962@gmail.com>
2023-11-14 20:05:41 +0700

    Update Product Categories for sliding
    - Sliding to view  Products in each Category is now available

--- app/src/main/java/com/example/testorder/Fragment/AllFragment.java
+++ app/src/main/java/com/example/testorder/Fragment/AllFragment.java
@@ -25,11 +25,8 @@ import com.squareup.picasso.Picasso;
 
 
 public class AllFragment extends Fragment {
-
-
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
 
     private String mParam1;
     private String mParam2;
